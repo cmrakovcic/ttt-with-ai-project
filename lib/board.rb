269218bc8a9d46efe4b8d5748ad508e80a508507
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-01-04 13:16:07 -0500

    commit tree 8049385509db966428a9e79a6227f2db03ff58e2

---
+++ lib/board.rb
@@ -0,0 +1,35 @@
+class Board
+    attr_accessor :cells
+    def initialize()
+        reset!
+    end
+    def reset!
+        @cells = Array.new(9, " ")
+    end
+    def display	
+        puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "	
+        puts "-----------"	
+        puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "	
+        puts "-----------"	
+        puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "	
+      end
+    def position(input)	
+        cells[input.to_i-1]	
+      end
+    def full?	
+        cells.all?{|token| token == "X" || token == "O"}
+    end
+    def turn_count	
+        cells.count{|token| token == "X" || token == "O"}
+    end
+    def taken?(position)
+        self.cells[position.to_i-1] == "X" ||
+        self.cells[position.to_i-1] == "O"
+      end
+    def valid_move?(position)
+        !taken?(position) && position.to_i >0 && position.to_i <=9
+    end
+    def update(position, player)
+        self.cells[position.to_i-1] = player.token
+    end
+end
